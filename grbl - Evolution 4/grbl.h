fcb4d0b89d140f3d1a432dc69ebab5a45d9a8bb2
Bob Wood <rlwoodjr@gmail.com>
2022-02-02 14:03:24 -0600

    Revert "Revert "Add all firmware changes""

--- grbl - Evolution 4/grbl.h
+++ grbl - Evolution 4/grbl.h
@@ -22,8 +22,9 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.1h"
-#define GRBL_VERSION_BUILD "20190830"
+#define GRBL_VERSION "1.1h Evolution 4" 
+
+#define GRBL_VERSION_BUILD "20220202"
 
 // Define standard libraries used by Grbl.
 #include <avr/io.h>
--- grbl - Revolution/cpu_map.h
+++ grbl - Revolution/cpu_map.h
@@ -37,17 +37,17 @@
   #define STEP_DDR        DDRD
   #define STEP_PORT       PORTD
   #define X_STEP_BIT      2  // Uno Digital Pin 2
-  #define Y_STEP_BIT      3  // Uno Digital Pin 3
+  #define A_STEP_BIT      3  // Uno Digital Pin 3
   #define Z_STEP_BIT      4  // Uno Digital Pin 4
-  #define STEP_MASK       ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
+  #define STEP_MASK       ((1<<X_STEP_BIT)|(1<<A_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
 
   // Define step direction output pins. NOTE: All direction pins must be on the same port.
   #define DIRECTION_DDR     DDRD
   #define DIRECTION_PORT    PORTD
   #define X_DIRECTION_BIT   5  // Uno Digital Pin 5
-  #define Y_DIRECTION_BIT   6  // Uno Digital Pin 6
+  #define A_DIRECTION_BIT   6  // Uno Digital Pin 6
   #define Z_DIRECTION_BIT   7  // Uno Digital Pin 7
-  #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
+  #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<A_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
 
   // Define stepper driver enable/disable output pin.
   #define STEPPERS_DISABLE_DDR    DDRB
@@ -61,14 +61,14 @@
   #define LIMIT_PIN        PINB
   #define LIMIT_PORT       PORTB
   #define X_LIMIT_BIT      1  // Uno Digital Pin 9
-  #define Y_LIMIT_BIT      2  // Uno Digital Pin 10
+  #define A_LIMIT_BIT      2  // Uno Digital Pin 10
   #ifdef VARIABLE_SPINDLE // Z Limit pin and spindle enabled swapped to access hardware PWM on Pin 11.
     #define Z_LIMIT_BIT	   4 // Uno Digital Pin 12
   #else
     #define Z_LIMIT_BIT    3  // Uno Digital Pin 11
   #endif
   #if !defined(ENABLE_DUAL_AXIS)
-    #define LIMIT_MASK     ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
+    #define LIMIT_MASK     ((1<<X_LIMIT_BIT)|(1<<A_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
   #endif
   #define LIMIT_INT        PCIE0  // Pin change interrupt enable pin
   #define LIMIT_INT_vect   PCINT0_vect
