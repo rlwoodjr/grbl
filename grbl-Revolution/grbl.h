19d9b39af0fd5f5009fdef10330188dd1823e5cd
rlwoodjr <rlwoodjr@gmail.com>
2022-02-12 08:43:11 -0600

    update firmware

--- grbl-Revolution/grbl.h
+++ grbl-Revolution/grbl.h
@@ -22,8 +22,8 @@
 #define grbl_h
 
 // Grbl versioning system
-#define GRBL_VERSION "1.1h"
-#define GRBL_VERSION_BUILD "20210102"
+#define GRBL_VERSION "1.1h Revolution"
+#define GRBL_VERSION_BUILD "20220202"
 
 
 
